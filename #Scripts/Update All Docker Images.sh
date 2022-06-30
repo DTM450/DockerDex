2df8e1d83b6f69201d361df1d74bfe3b35fbc9ee
DTM450 <dtm450ongithub@gmail.com>
2022-06-30 13:52:28 +0930

    Update Update All Docker Images.sh
    Added logging to the script, cleanly shows what image was updated.

--- #Scripts/Update All Docker Images.sh
+++ #Scripts/Update All Docker Images.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker images --format "{{.Repository}}:{{.Tag}}" | for line in $(grep ":latest"); do docker pull $line; done
+docker images --format "{{.Repository}}:{{.Tag}}" | for line in $(grep ":latest"); do docker pull $line | grep "Status: " >> /var/log/dockerdex.log; done
